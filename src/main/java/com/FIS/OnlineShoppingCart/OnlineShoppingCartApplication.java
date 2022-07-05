c8f3529297f56421e465dd9ff4f3bc9efd8f11ab
HoangNH102 <Hoangnh180897@gmail.com>
2022-07-05 10:57:09 +0700

    Lan Dau Tien HoangNH102

--- src/main/java/com/FIS/OnlineShoppingCart/OnlineShoppingCartApplication.java
+++ src/main/java/com/FIS/OnlineShoppingCart/OnlineShoppingCartApplication.java
@@ -8,6 +8,7 @@ public class OnlineShoppingCartApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OnlineShoppingCartApplication.class, args);
+		System.out.println("Hoang Den Day !");
 	}
 
 }
